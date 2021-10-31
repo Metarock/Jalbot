764749dc808d0c66f977e4aabd8631ec836d542f
Metarock <esang037@gmail.com>
2021-10-31 20:40:32 +1300

    added status

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,5 +2,9 @@ const chalk = require('chalk');
 
 module.exports = (client) => {
     client.manager.init(client.user.id);
+    client.user.setActivity({
+        name: `Ming mong`,
+        type: "LISTENING",
+    })
     console.log(`${chalk.bold.green('[BOT]')} Logged in as ${client.user.username}#${client.user.discriminator}.`);
 };
